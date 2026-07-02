514603a1141680d508807a9864d9a4419aac3fba
Charles Wright <charles.tjw@gmail.com>
2026-07-02 08:26:50 +0000

    Add GUI runner for device type component sync

--- netbox_sync/views.py
+++ netbox_sync/views.py
@@ -1,5 +1,25 @@
+from io import StringIO
+
+from django.core.management import call_command
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "netbox_sync/home.html")
+    output = None
+
+    if request.method == "POST":
+        commit = request.POST.get("commit") == "on"
+
+        buffer = StringIO()
+
+        call_command(
+            "sync_device_type_components",
+            stdout=buffer,
+            commit=commit,
+        )
+
+        output = buffer.getvalue()
+
+    return render(request, "netbox_sync/home.html", {
+        "output": output,
+    })
