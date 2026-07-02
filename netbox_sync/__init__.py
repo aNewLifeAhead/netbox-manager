1e937edeaba72f7dde5c240ec2fecdbe01a0a1ac
Charles Wright <charles.tjw@gmail.com>
2026-07-02 10:53:19 +0000

    SYNC

--- netbox_sync/__init__.py
+++ netbox_sync/__init__.py
@@ -4,8 +4,10 @@ from netbox.plugins import PluginConfig
 class NetBoxSyncConfig(PluginConfig):
     name = "netbox_sync"
     verbose_name = "NetBox Sync"
-    description = "Custom NetBox sync utilities"
-    version = "0.1.0"
+    description = "Device Type component synchronisation tool for NetBox"
+    version = "0.1.1"
+    author = "Charles Wright"
+    author_email = "charlie@fix8group.com"
     base_url = "https://github.com/aNewLifeAhead/netbox-sync"
 
 
